2184a1f0553026476292dedeeb3584f32640ebeb
kingshuk111 <38248221+kingshuk111@users.noreply.github.com>
2021-05-11 18:12:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello Cloud Gladiators"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello Cloud Gladiators!"
   });
 });
 
